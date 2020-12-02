2e6fb6a9e98b9427210f3e4ada1bbe207a5ad97b
Ulises Santana <ulisesantana@gmail.com>
2020-12-02 01:18:58 +0000

    ✅ resolves day 1 puzzles

---
+++ src/day1/expenseReport.test.ts
@@ -0,0 +1,17 @@
+import {
+  processExpensesForTwoEntries,
+  processExpensesForThreeEntries,
+} from "./expenseReport";
+describe("Expenses report should", () => {
+  it("find the two entries that sum to 2020 and multiply them together", () => {
+    const expenses = [1721, 979, 366, 299, 675, 1456];
+
+    expect(processExpensesForTwoEntries(expenses)).toBe(514579);
+  });
+
+  it("find the three entries that sum to 2020 and multiply them together", () => {
+    const expenses = [1721, 979, 366, 299, 675, 1456];
+
+    expect(processExpensesForThreeEntries(expenses)).toBe(241861950);
+  });
+});
