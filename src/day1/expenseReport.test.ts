96d030a7b32a0e60847666a606221021ce0e8bc3
Ulises Santana <ulisesantana@gmail.com>
2020-12-02 12:23:22 +0000

    🚨 fix linter warnings

--- src/day1/expenseReport.test.ts
+++ src/day1/expenseReport.test.ts
@@ -1,17 +1,18 @@
 import {
   processExpensesForTwoEntries,
-  processExpensesForThreeEntries,
-} from "./expenseReport";
-describe("Expenses report should", () => {
-  it("find the two entries that sum to 2020 and multiply them together", () => {
-    const expenses = [1721, 979, 366, 299, 675, 1456];
+  processExpensesForThreeEntries
+} from './expenseReport'
 
-    expect(processExpensesForTwoEntries(expenses)).toBe(514579);
-  });
+describe('Expenses report should', () => {
+  it('find the two entries that sum to 2020 and multiply them together', () => {
+    const expenses = [1721, 979, 366, 299, 675, 1456]
 
-  it("find the three entries that sum to 2020 and multiply them together", () => {
-    const expenses = [1721, 979, 366, 299, 675, 1456];
+    expect(processExpensesForTwoEntries(expenses)).toBe(514579)
+  })
 
-    expect(processExpensesForThreeEntries(expenses)).toBe(241861950);
-  });
-});
+  it('find the three entries that sum to 2020 and multiply them together', () => {
+    const expenses = [1721, 979, 366, 299, 675, 1456]
+
+    expect(processExpensesForThreeEntries(expenses)).toBe(241861950)
+  })
+})
--- src/day1/expenseReport.ts
+++ src/day1/expenseReport.ts
@@ -1,15 +1,15 @@
-import expenses from "./input.json";
+import expenses from './input.json'
 
 export function processExpensesForTwoEntries(expenses: number[]): number {
   for (const expenseA of expenses) {
     const expenseB = expenses.find(
       (expenseB: number) => expenseA + expenseB === 2020
-    );
+    )
     if (expenseB !== undefined) {
-      return expenseA * expenseB;
+      return expenseA * expenseB
     }
   }
-  return -1;
+  return -1
 }
 
 export function processExpensesForThreeEntries(expenses: number[]): number {
@@ -17,26 +17,26 @@ export function processExpensesForThreeEntries(expenses: number[]): number {
     for (const expenseB of expenses) {
       const expenseC = expenses.find(
         (expenseC: number) => expenseA + expenseB + expenseC === 2020
-      );
+      )
       if (expenseC !== undefined) {
-        return expenseA * expenseB * expenseC;
+        return expenseA * expenseB * expenseC
       }
     }
   }
-  return -1;
+  return -1
 }
 
 if (require.main === module) {
-  const start = Date.now();
+  const start = Date.now()
   console.log(
     `The result of the expenses report for 2 entries is ${processExpensesForTwoEntries(
       expenses
     )}`
-  );
+  )
   console.log(
     `The result of the expenses report for 3 entries is ${processExpensesForThreeEntries(
       expenses
     )}`
-  );
-  console.log(`Elapsed time: ${Date.now() - start} ms.`);
+  )
+  console.log(`Elapsed time: ${Date.now() - start} ms.`)
 }
